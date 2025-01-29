b77a181268fc23c60e99d882da65515a2634742d
Clement Bouvet <clement@cbouvet.fr>
2025-01-29 10:19:26 +0100

    clippy pass

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ impl Guest for Component {
     fn page(_edgee_event: Event, _cred_map: Dict) -> Result<EdgeeRequest, String> {
         Ok(EdgeeRequest {
             method: HttpMethod::Post,
-            url: format!("https://example.com/page"),
+            url: format!("https://example.com/{}", "page"),
             headers: Vec::new(),
             body: String::new(),
         })
@@ -19,7 +19,7 @@ impl Guest for Component {
     fn track(_edgee_event: Event, _cred_map: Dict) -> Result<EdgeeRequest, String> {
         Ok(EdgeeRequest {
             method: HttpMethod::Post,
-            url: format!("https://example.com/track"),
+            url: format!("https://example.com/{}", "track"),
             headers: Vec::new(),
             body: String::new(),
         })
@@ -28,7 +28,7 @@ impl Guest for Component {
     fn user(_edgee_event: Event, _cred_map: Dict) -> Result<EdgeeRequest, String> {
         Ok(EdgeeRequest {
             method: HttpMethod::Post,
-            url: format!("https://example.com/user"),
+            url: format!("https://example.com/{}", "user"),
             headers: Vec::new(),
             body: String::new(),
         })
