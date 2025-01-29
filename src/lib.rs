4cbcbea36e1bdbc0db1ea9677fd6a10e01c37a58
Clement Bouvet <clement@cbouvet.fr>
2025-01-29 10:20:32 +0100

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,18 @@ export!(Component);
 
 struct Component;
 
+/*
+* Implement the Guest trait for the Component struct
+* to create the required functions for the data collection protocol
+* for your provider.
+* The functions are page, track, and user.
+* The page function is called when the page event is triggered.
+* The track function is called when the track event is triggered.
+* The user function is called when the user event is triggered.
+* The functions should return an EdgeeRequest or an error message.
+* The EdgeeRequest contains the method, url, headers, and body of the request.
+*/
+
 impl Guest for Component {
     fn page(_edgee_event: Event, _cred_map: Dict) -> Result<EdgeeRequest, String> {
         Ok(EdgeeRequest {
