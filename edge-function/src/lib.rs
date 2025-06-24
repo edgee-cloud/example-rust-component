d413876fbaa1d8f72349ad2e7ce3bd514c7e1dde
Clement Bouvet <clement@cbouvet.fr>
2025-06-24 21:49:27 +0200

    thinking

--- edge-function/src/lib.rs
+++ edge-function/src/lib.rs
@@ -6,60 +6,71 @@ use exports::wasi::http::incoming_handler::{Guest, ResponseOutparam};
 use wasi::http::types::IncomingRequest;
 use wasi::http::types::{Fields, OutgoingBody, OutgoingResponse};
 
-use wasi::http::outgoing_handler::OutgoingRequest;
+pub struct ResponseBuilder {
+    headers: Fields,
+    status_code: u16,
+    body_content: Option<String>,
+}
 
-use url::Url;
+impl ResponseBuilder {
+    pub fn new() -> Self {
+        ResponseBuilder {
+            headers: Fields::new(),
+            status_code: 200,
+            body_content: None,
+        }
+    }
+
+    pub fn set_header(&mut self, key: &str, value: &str) -> &mut Self {
+        let _ = self
+            .headers
+            .set(key, vec![value.as_bytes().to_vec()].as_slice());
+        self
+    }
+
+    pub fn set_status_code(&mut self, status_code: u16) -> &mut Self {
+        self.status_code = status_code;
+        self
+    }
+
+    pub fn set_body(&mut self, body: &str) -> &mut Self {
+        self.body_content = Some(body.to_string());
+        self
+    }
+
+    pub fn build(self, resp: ResponseOutparam) {
+        let mut resp_tx = OutgoingResponse::new(self.headers);
+        let _ = resp_tx.set_status_code(self.status_code);
+
+        if let Some(body_content) = self.body_content {
+            let body = resp_tx.body().unwrap();
+            ResponseOutparam::set(resp, Ok(resp_tx));
+
+            let mut stream = body.write().unwrap();
+            stream
+                .blocking_write_and_flush(body_content.as_bytes())
+                .unwrap();
+            drop(stream);
+
+            let _ = OutgoingBody::finish(body, None);
+        }
+    }
+}
 
 impl Guest for Component {
     fn handle(req: wasi::http::types::IncomingRequest, resp: wasi::http::types::ResponseOutparam) {
         let incoming_headers = IncomingRequest::headers(&req);
-        //let settings_headers = incoming_headers.get("X-Edgee-Settings");
-        //// Parse the settings from the headers
-        //if settings_headers.len() != 1 {
-        //    panic!("Multiple X-Edgee-Settings headers found");
-        //}
-        //let settings = settings_headers.get(0).cloned();
-        //let settings = match settings {
-        //    Some(settings) => String::from_utf8_lossy(&settings).to_string(),
-        //    None => {
-        //        panic!("X-Edgee-Settings header not found");
-        //    }
-        //};
-        //let settings = Settings::new(settings).unwrap();
-        // let incoming_body = IncomingRequest::consume(&req).unwrap();
-        // let incoming_body_stream = incoming_body.stream().unwrap();
-        //       let body = incoming_body_stream.read().unwrap();
-        let response_headers = Fields::new();
 
-        let _ = response_headers.set(
-            "content-type",
-            vec!["text/html".as_bytes().to_vec()].as_slice(),
-        );
-
-        let _ = response_headers.set(
-            "content-length",
-            vec![include_str!("index.html")
-                .len()
-                .to_string()
-                .as_bytes()
-                .to_vec()]
-            .as_slice(),
-        );
-
-        let index = include_str!("index.html");
-
-        // stream
-        let resp_tx = OutgoingResponse::new(response_headers);
-        let _ = resp_tx.set_status_code(200);
-        let body = resp_tx.body().unwrap();
-        ResponseOutparam::set(resp, Ok(resp_tx));
-        // stream the response body
-        let stream = body.write().unwrap();
-        stream.write(index.as_bytes()).unwrap();
-        // finish the response -> drop flushes the stream
-        drop(stream);
-        // this tells the host that the response is complete
-        let _ = OutgoingBody::finish(body, None);
+        let mut builder = ResponseBuilder::new();
+        builder
+            .set_header("content-type", "text/html")
+            .set_header(
+                "content-length",
+                &include_str!("index.html").len().to_string(),
+            )
+            .set_status_code(200)
+            .set_body(include_str!("index.html"));
+        builder.build(resp);
     }
 }
 
